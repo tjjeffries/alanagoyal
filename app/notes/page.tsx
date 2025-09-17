dbe40a32a02a6e71df829f9d67f57ed7fe35ebf5
tjjeffries <tjjeffries03@g.ucla.edu>
2025-09-16 21:51:54 -0700

    Redirect /notes to about-me

--- app/notes/page.tsx
+++ app/notes/page.tsx
@@ -1,12 +1,18 @@
+import { redirect } from "next/navigation";
 import { Metadata } from "next";
 
 export async function generateMetadata(): Promise<Metadata> {
   return {
     title: "notes",
     openGraph: {
-      images: [`/notes/api/og/?title=${encodeURIComponent("notes")}&emoji=${encodeURIComponent("✏️")}`],
+      images: [
+        `/notes/api/og/?title=${encodeURIComponent("notes")}&emoji=${encodeURIComponent("✏️")}`,
+      ],
     },
   };
 }
 
-export default async function Home() {}
+export default function NotesIndexPage() {
+  // change "about-me" to whatever your default slug is
+  return redirect("/notes/about-me");
+}
